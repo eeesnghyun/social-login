59203e421bef2ee1d538d6af2caea58ac1e8361f
wwl-lsh <lsh@winwinlab.co.kr>
2023-03-21 11:43:19 +0900

    initial commit

---
+++ src/test/java/com/app/sociallogin/SocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.sociallogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
